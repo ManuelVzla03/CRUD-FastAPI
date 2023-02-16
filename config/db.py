2c105d57d560d470b34f6800433f49f5e251acb4
Manuel De Jesus Valenzuela Vazquez <manuel.valenzuela216785@potros.itson.edu.mx>
2023-02-16 11:00:59 -0700

    Metodo creados y finalizados.
    Los métodos html fueron finalizados.

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/store_db")
+engine = create_engine("mysql+pymysql://root:Manuel123*@localhost:3306/store_db")
 
 meta = MetaData()
 
